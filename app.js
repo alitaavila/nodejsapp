ee30ace9fe1823a3f23becff46ad8ea07b696d62
Alejandra Maria Avila Perez <54506203+alitaavila@users.noreply.github.com>
2023-03-02 09:21:19 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! Hola Mundo!');
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App escuchando a http://%s:%s', host, port);
 });
